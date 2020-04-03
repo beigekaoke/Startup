d991dbed13bff81142948dda59213df5c9951699
beigekaoke <1242672540@qq.com>
2020-04-03 20:26:15 +0800

    add a Interger cache test

---
+++ src/test/java/com/flagship/Startup/utils/SomeTest.java
@@ -0,0 +1,18 @@
+package com.flagship.startup.utils;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author: flag ship
+ * @Date: 2020/3/28 19:34
+ */
+public class SomeTest {
+
+    @Test
+    public void test1(){
+        Integer f1 = 100, f2 = 100, f3 = 150, f4 = 150;
+        System.out.println(f1 == f2);
+        System.out.println(f3 == f4);
+    }
+
+}
