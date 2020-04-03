9427000c05995f04026ab6313d1eb13c392d7b45
beigekaoke <1242672540@qq.com>
2020-04-03 23:01:58 +0800

    add swagger support

--- src/main/java/com/flagship/startup/controller/HelloWorldController.java
+++ src/main/java/com/flagship/startup/controller/HelloWorldController.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +19,7 @@ import com.flagship.startup.entity.Location;
 import com.flagship.startup.repository.LocationRepository;
 import com.flagship.startup.websocket.WebSocketServer;
 
+@Api(value="Hello world controller",tags={"Websocket operation api"})
 @RestController
 public class HelloWorldController {
 	
@@ -26,6 +31,8 @@ public class HelloWorldController {
 		return "Hello world!";
 	}
 
+	@ApiOperation(value = "push video", notes="push video to webd")
+	@ApiImplicitParam(name = "account", value = "账户", paramType = "query", required = true, dataType = "json")
 	@RequestMapping(value = "/pushVideoListToWeb", method = RequestMethod.POST, consumes = "application/json")
 	public @ResponseBody Map<String, Object> pushVideoListToWeb(@RequestBody Map<String, Object> param) {
 		Map<String, Object> result = new HashMap<String, Object>();
