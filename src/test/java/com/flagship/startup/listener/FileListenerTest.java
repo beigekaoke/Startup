61d50f185308478c7a03d80c1f90751c6bd1f41c
beigekaoke <1242672540@qq.com>
2020-04-15 22:50:59 +0800

    add file listener

---
+++ src/test/java/com/flagship/startup/listener/FileListenerTest.java
@@ -0,0 +1,18 @@
+package com.flagship.startup.listener;
+
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: flag ship
+ * @Date: 2020/4/15 22:30
+ */
+public class FileListenerTest {
+
+        }
