6f455af57ec3d86b348c760d10c790abbad87fd3
beigekaoke <1242672540@qq.com>
2020-05-11 23:27:02 +0800

    auto format date

--- src/test/java/com/flagship/startup/utils/SomeTest.java
+++ src/test/java/com/flagship/startup/utils/SomeTest.java
@@ -2,6 +2,10 @@ package com.flagship.startup.utils;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+
+import static com.flagship.startup.utils.DateUtils.getAutoDate;
+
 /**
  * @Author: flag ship
  * @Date: 2020/3/28 19:34
@@ -9,10 +13,25 @@ import org.junit.jupiter.api.Test;
 public class SomeTest {
 
     @Test
-    public void test1(){
+    public void test1() {
         Integer f1 = 100, f2 = 100, f3 = 150, f4 = 150;
+        int ii = 32;
         System.out.println(f1 == f2);
         System.out.println(f3 == f4);
+
+        String ss = "weqwrqwer";
+        byte[] bbs = ss.getBytes();
+    }
+
+    @Test
+    public void testDateFormat() throws ParseException {
+        System.out.println(getAutoDate("1987-11-04"));
+        System.out.println(getAutoDate("1987-11-04 12:50"));
+        System.out.println(getAutoDate("1987-11-04 12:50:15"));
+        System.out.println(getAutoDate("1987-11-04 12:50:15.000"));
+        System.out.println(getAutoDate("19871104"));
+        System.out.println(getAutoDate("04/11/1987"));
+        System.out.println(getAutoDate("1/1/1987"));
     }
 
 }
